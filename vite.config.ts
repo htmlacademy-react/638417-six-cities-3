16b1446929f80db4b5608ecc4fc1f792a9c75c28
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2025-04-10 20:01:06 +0300

    Vite open local true

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // <-- Эта строка открывает вкладку в браузере
+  },
   test: {
     globals: true,
     environment: 'jsdom',
