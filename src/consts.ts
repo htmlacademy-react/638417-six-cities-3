15d36862b8508133a50e2a64d9709364d60d77e1
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2025-04-28 18:34:59 +0300

    Init offer form

--- src/consts.ts
+++ src/consts.ts
@@ -16,3 +16,11 @@ export enum AuthorizationStatus {
 }
 
 export const RATING_MAX = 5;
+
+export const RATINGS = [
+  { value: 5, title: 'perfect' },
+  { value: 4, title: 'good' },
+  { value: 3, title: 'not bad' },
+  { value: 2, title: 'badly' },
+  { value: 1, title: 'terribly' },
+];
