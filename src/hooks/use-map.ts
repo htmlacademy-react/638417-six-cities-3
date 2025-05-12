56a9c8852e46c7615ace9adfc991e830af8a26a7
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2025-05-12 14:21:42 +0300

    1.1.0 Map points by currentCity

--- src/hooks/use-map.ts
+++ src/hooks/use-map.ts
@@ -1,21 +1,22 @@
-import { useEffect, useRef } from 'react';
 import leaflet, { Map as LeafletMap } from 'leaflet';
 import { TLocation } from '../types/offers';
+import { useEffect, useRef, useState } from 'react';
 
 type UseMapProps = {
   location: TLocation;
   containerRef: React.RefObject<HTMLElement | null>;
-};
+}
 
 const TILE_LAYER_PATTERN = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
 const TILE_LAYER_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
 export const useMap = ({ location, containerRef }: UseMapProps): LeafletMap | null => {
-  const mapRef = useRef<LeafletMap | null>(null);
+  const [map, setMap] = useState<LeafletMap | null>(null);
+  const isRenderRef = useRef(false);
 
   useEffect(() => {
-    if (containerRef.current !== null && mapRef.current === null) {
-      const mapInstance = leaflet.map(containerRef.current, {
+    if (containerRef.current !== null && !isRenderRef.current) {
+      const instance = leaflet.map(containerRef.current, {
         center: {
           lat: location.latitude,
           lng: location.longitude,
@@ -27,17 +28,14 @@ export const useMap = ({ location, containerRef }: UseMapProps): LeafletMap | nu
         .tileLayer(TILE_LAYER_PATTERN, {
           attribution: TILE_LAYER_ATTRIBUTION,
         })
-        .addTo(mapInstance);
+        .addTo(instance);
 
-      mapRef.current = mapInstance;
+      setMap(instance);
+      isRenderRef.current = true;
+    } else if (map) {
+      map.setView([location.latitude, location.longitude], location.zoom);
     }
-  }, [containerRef, location]);
+  }, [location, containerRef, map]);
 
-  useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.setView([location.latitude, location.longitude], location.zoom);
-    }
-  }, [location]);
-
-  return mapRef.current;
+  return map;
 };
