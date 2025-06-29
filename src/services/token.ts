5638018a26c8d92cc47c5c5621f969d72ceab546
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2025-06-29 20:09:02 +0300

    7.4 add Tocken support

--- src/services/token.ts
+++ src/services/token.ts
@@ -2,15 +2,6 @@ const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
 
 export type Token = string;
 
-export const getToken = (): Token => {
-  const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
-  return token ?? 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20=';
-};
-
-export const saveToken = (token: Token): void => {
-  localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
-};
-
-export const dropToken = (): void => {
-  localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
-};
+export const getToken = (): Token => localStorage.getItem(AUTH_TOKEN_KEY_NAME) ?? 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20=';
+export const saveToken = (token: Token): void => localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+export const dropToken = (): void => localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
