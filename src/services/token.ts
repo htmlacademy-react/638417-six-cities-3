a713e6c2bff64979cdf14311e9f68e5eb6d5eecb
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2025-06-29 20:05:27 +0300

    7.2 init Thunk + 7.3 Token functions

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,3 +1,16 @@
-const getToken = (): string => 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20=';
+const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
 
-export default getToken;
+export type Token = string;
+
+export const getToken = (): Token => {
+  const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
+  return token ?? 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20=';
+};
+
+export const saveToken = (token: Token): void => {
+  localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+};
+
+export const dropToken = (): void => {
+  localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+};
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import { createAPI as api } from '../services/api';
 import { filterReducer } from './slices/filter';
 import { offersReducer } from './slices/offers';
 import { configureStore } from '@reduxjs/toolkit';
@@ -6,5 +7,11 @@ export const store = configureStore({
   reducer: {
     offers: offersReducer,
     filter: filterReducer
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
